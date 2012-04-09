ef5d4ea347a0402b7cc68823af66850eb540e048
Thomas Castiglione <castiglione@gmail.com>
2012-04-09 22:00:51 +0800

    some useful assembler testcases

---
+++ test/labels.s
@@ -0,0 +1,7 @@
+			SET A, 0x30		; 7c01 0030
+:a			SET [0x1000], 0x20	; 7de1 1000 0020
+
+:b
+:c			SUB A, [0x1000]		; 7803 1000
+			IFN A, 0x10		; c00d 
+	:d		SET PC, crash		; 7dc1 001a [*]
---
+++ test/values.s
@@ -0,0 +1,11 @@
+set a, 0x1000
+set a, b
+set a, [0x1000]
+set [a], 0x1000
+set [a], b
+set [a], [0x1000]
+set [a+10], [a+0x10]
+set push, 0x1000
+set c, pop
+set sp, peep
+			
